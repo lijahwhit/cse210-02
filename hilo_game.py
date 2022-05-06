4c7cd9ac09e798cdad247a66121c76481134d502
kylecooltron <kylejcoulon@gmail.com>
2022-05-05 18:40:03 -0600

    we made a class for the game loop, and a card class

--- hilo_game.py
+++ hilo_game.py
@@ -6,21 +6,38 @@
 import random
 
 
+class Card:
+
+    def __init__(self):
+        self.number = 0
+
+    def randomize(self):
+        self.number = random.randrange(1, 14)
+
+
 class Hilo:
 
     def __init__(self):
         self.score = 0
+        self.card = Card()
 
     def start(self):
         """Description: starts a new game
         """
 
         # set initial parameters for new game
+        self.score = 300
 
         while True:
             # game play
+            self.card.randomize()
 
-            print()
+            print(f"The current card is: {self.card.number}")
+
+            user_input = input("choose Y/N")
+
+            if(user_input == "N"):
+                break
 
         # when the game loop is broken, start a new game
         self.start()
